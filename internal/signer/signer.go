d5c2736a5638682eec5b650bc383cd7e07b1248b
Pritesh Bandi <pritesb@amazon.com>
2024-03-15 12:52:01 -0700

    chore: add license and .github files
    Signed-off-by: Pritesh Bandi <priteshbandi@gmail.com>

--- internal/signer/signer.go
+++ internal/signer/signer.go
@@ -1,3 +1,16 @@
+//  Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"). You may
+//  not use this file except in compliance with the License. A copy of the
+//  License is located at
+//
+// 	http://aws.amazon.com/apache2.0
+//
+//  or in the "license" file accompanying this file. This file is distributed
+//  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+//  express or implied. See the License for the specific language governing
+//  permissions and limitations under the License.
+
 // Package signer provides functionality to generate signatures using AWS Signer
 // in accordance with the NotaryProject Plugin contract.
 package signer
@@ -14,8 +27,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/aws-sdk-go-v2/service/signer"
-	"github.com/aws/smithy-go"
-
 	"github.com/notaryproject/notation-plugin-framework-go/plugin"
 )
 
