88e9eb91354b84b7f3bb4808c19244ea2cd45e18
Aparna Reddy <areddy.pothula@gmail.com>
2024-03-19 20:27:56 -0700

    add version and .gitignore

--- internal/version/version.go
+++ internal/version/version.go
@@ -14,10 +14,9 @@
 // Package version provides utility methods for AWS Signer's plugin version.
 package version
 
-// Version value will be set during the build.
-var Version = "unknown"
+var version = "1.0.300"
 
 // GetVersion returns the plugin version in Semantic Versioning 2.0.0 format.
 func GetVersion() string {
-	return Version
+	return version
 }
